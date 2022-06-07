0a584d05b06d1f9136dbd45ef64e0df0f9f305e8
unknown <wendyli8620lw@gmail.com>
2022-06-07 20:49:41 +0200

    init OpenApiDoc

---
+++ OpenApiDoc/src/main/java/com/example/OpenApiDoc/OpenApiDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenApiDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiDocApplication.class, args);
+	}
+
+}
---
+++ OpenApiDoc/src/test/java/com/example/OpenApiDoc/OpenApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenApiDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
