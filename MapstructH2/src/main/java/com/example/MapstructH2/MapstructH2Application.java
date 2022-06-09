8b085739e9d7a3e38afe1b220a3bc3fcec05758e
unknown <wendyli8620lw@gmail.com>
2022-06-09 17:38:48 +0200

    init mapstruct project

---
+++ MapstructH2/src/main/java/com/example/MapstructH2/MapstructH2Application.java
@@ -0,0 +1,13 @@
+package com.example.MapstructH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructH2Application.class, args);
+	}
+
+}
---
+++ MapstructH2/src/test/java/com/example/MapstructH2/MapstructH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MapstructH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
