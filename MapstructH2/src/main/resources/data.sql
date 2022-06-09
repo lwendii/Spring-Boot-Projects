edb27b02bcefdb131d4e345423cc11d334e567d8
unknown <wendyli8620lw@gmail.com>
2022-06-09 17:40:28 +0200

    added h2 config

---
+++ MapstructH2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, firstname, lastname, email) VALUES (1, 'Jane', 'Doe', 'jane.doe@thismail1.com');
+INSERT INTO customer (id, firstname, lastname, email) VALUES (2, 'John', 'Doe', 'john.doe@thismail1.com');
+INSERT INTO customer (id, firstname, lastname, email) VALUES (3, 'Stewart', 'Little', 's.little@thismail1.com');
+INSERT INTO customer (id, firstname, lastname, email) VALUES (4, 'Doug', 'Henney', 'doug.h@thismail1.com');
+INSERT INTO customer (id, firstname, lastname, email) VALUES (5, 'Prima', 'Doll', 'p.doll@thismail1.com');
