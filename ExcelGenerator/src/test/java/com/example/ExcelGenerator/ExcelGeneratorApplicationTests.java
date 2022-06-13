70ff0aa121ffcce1652ea0eecd5b9d8f6fcb6c01
unknown <wendyli8620lw@gmail.com>
2022-06-13 22:08:30 +0200

    init proj

---
+++ ExcelGenerator/src/test/java/com/example/ExcelGenerator/ExcelGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExcelGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
